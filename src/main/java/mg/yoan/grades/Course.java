c8ef854929b6d4167c2c705777cce88c839bce7e
Yoan <yoandiny14@gmail.com>
2025-09-04 08:27:30 +0300

    feat: added Course and Exam structure

--- src/main/java/mg/yoan/grades/Course.java
+++ src/main/java/mg/yoan/grades/Course.java
@@ -1,4 +1,9 @@
 package mg.yoan.grades;
 
 public class Course {
+    private int id;
+    private String label;
+    private int credits;
+    private Teacher teacher;
+
 }
--- src/main/java/mg/yoan/grades/Exam.java
+++ src/main/java/mg/yoan/grades/Exam.java
@@ -1,4 +1,11 @@
 package mg.yoan.grades;
 
+import java.time.Instant;
+
 public class Exam {
+    private int id;
+    private String title;
+    private Course course;
+    private Instant examDate;
+    private int coefficient;
 }
