a345bf8b6ba64d57272ac0873d658c5851a6f66f
Yoan <yoandiny14@gmail.com>
2025-09-04 09:57:12 +0300

    fix: modified getCourseGrade calculation

--- src/main/java/mg/yoan/grades/Grade.java
+++ src/main/java/mg/yoan/grades/Grade.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.Instant;
+import java.util.ArrayList;
 
 @AllArgsConstructor
 @Getter
@@ -14,17 +15,21 @@ public class Grade {
     private double grade;
 
 
+    double getExamGrade(Exam exam, Student student, Instant t) {
+        return
+
+    }
+
+
     double getCourseGrade(Course course, Student student, Instant t) {
         return course.getGrades().stream()
                 .filter(g -> g.getStudent() == student)
                 .map(g -> g.getDate().isBefore(t))
                 .mapToDouble(g ->getGrade())
-                .reduce(0, (subtotal, element) -> subtotal + element);
+                .reduce(0, (subtotal, element) -> subtotal + element)/course.getGrades().size();
 
     }
 
-    double getExamGrade(Exam exam, Student student, Instant t) {
 
-    }
 
 }
