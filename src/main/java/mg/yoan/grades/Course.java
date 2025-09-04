57e2f29066b2811194b962bb9ac203a9f0f448fc
Yoan <yoandiny14@gmail.com>
2025-09-04 09:33:11 +0300

    feat: added getCourseGrade function

--- src/main/java/mg/yoan/grades/Course.java
+++ src/main/java/mg/yoan/grades/Course.java
@@ -1,9 +1,21 @@
 package mg.yoan.grades;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
 public class Course {
     private int id;
     private String label;
     private int credits;
     private Teacher teacher;
+    private List<Grade> grades;
+
+
 
 }
--- src/main/java/mg/yoan/grades/Grade.java
+++ src/main/java/mg/yoan/grades/Grade.java
@@ -1,4 +1,24 @@
 package mg.yoan.grades;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@Getter
 public class Grade {
+    private Student student;
+    private Instant date;
+    private int grade;
+
+
+    double getCourseGrade(Course course, Student student, Instant t) {
+        return course.getGrades().stream()
+                .filter(g -> g.getStudent() == student)
+                .mapToDouble(g -> g.getGrade())
+                .reduce(0, (subtotal, element) -> subtotal + element);
+
+    }
 }
