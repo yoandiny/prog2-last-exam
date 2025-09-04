652d1d7a6c5e3c2d8062635715becba47bc937dc
Yoan <yoandiny14@gmail.com>
2025-09-04 08:23:33 +0300

    feat: added Teacher structure

--- src/main/java/mg/yoan/grades/Teacher.java
+++ src/main/java/mg/yoan/grades/Teacher.java
@@ -1,4 +1,13 @@
 package mg.yoan.grades;
 
+import java.time.Instant;
+
 public class Teacher {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private Instant birthDate;
+    private String email;
+    private String phone;
+    private String specialty;
 }
