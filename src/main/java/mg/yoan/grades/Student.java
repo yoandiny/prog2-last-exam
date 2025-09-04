829149742dcc3523126d46dc15495361c4259af0
Yoan <yoandiny14@gmail.com>
2025-09-04 08:20:03 +0300

    feat: added Student and Tutor structure

--- src/main/java/mg/yoan/grades/Student.java
+++ src/main/java/mg/yoan/grades/Student.java
@@ -3,6 +3,16 @@ package mg.yoan.grades;
 
 import lombok.AllArgsConstructor;
 
+import java.time.Instant;
+
 @AllArgsConstructor
 public class Student {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private Instant birthDate;
+    private String email;
+    private String phone;
+    private String group;
+    private Tutor tutor;
 }
--- src/main/java/mg/yoan/grades/Tutor.java
+++ src/main/java/mg/yoan/grades/Tutor.java
@@ -1,4 +1,17 @@
 package mg.yoan.grades;
 
+import lombok.AllArgsConstructor;
+
+import java.time.Instant;
+
+@AllArgsConstructor
 public class Tutor {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private Instant birthDate;
+    private String email;
+    private String phone;
+    private String linkDescription;
+
 }
