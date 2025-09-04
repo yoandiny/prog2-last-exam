e3b0781cc4ac1b04465587e2a82b17fe882606f9
Yoan <yoandiny14@gmail.com>
2025-09-04 09:25:22 +0300

    feat: added Course and Exam Test files

---
+++ src/test/java/mg/yoan/grades/CourseTest.java
@@ -0,0 +1,12 @@
+package mg.yoan.grades;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @Test
+    void getCourseGrade() {
+    }
+}
